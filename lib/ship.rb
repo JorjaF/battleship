632c8ca4b4b51e4487893eae0b46c470d47a8697
Lauren Golden <llgolden@gmail.com>
2023-04-06 15:40:45 -0600

    Add health and sunk? methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,24 @@
 class Ship
+  attr_reader :name 
+              :length
 
   def initialize(name, length)
     @name = name
     @length = length
+    
   end
+
+  def health
+    @length
+  end
+
+  def sunk?
+    if health > 0
+      false
+    else 
+      true
+    end
+  end
+
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,30 +1,38 @@
 require 'rspec'
 require './lib/ship'
 
-
 RSpec.describe Ship do
-  it 'exists' do
+  it "exists" do
     expect(cruiser = Ship.new("Cruiser", 3)).to be_a(Ship)
   end
+
+  it "has attributes" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq("Cruiser")
+    # expect(cruiser.length).to eq(3)
+  end
+
+  it "has health" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.health).to eq (3)
+  end
+
+  it "can sink" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.sunk?).to eq (false)
+    
+  end
+
+
 
 end
 
 
 
-# cruiser = Ship.new("Cruiser", 3)
-# #=> #<Ship:0x00007feb05112d10...>
 
-# cruiser.name
-# #=> "Cruiser"
 
-# cruiser.length
-# #=> 3
 
-# cruiser.health
-# #=> 3
 
-# cruiser.sunk?
-# #=> false
 
 # cruiser.hit
 
