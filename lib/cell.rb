bb117d993bc6e04391dd42af13ec6802ba98f017
Lauren Golden <llgolden@gmail.com>
2023-04-06 21:12:52 -0600

    Set up cell class and spec

---
+++ lib/cell.rb
@@ -0,0 +1,10 @@
+class Cell
+  # attr_reader :name 
+  #             :length
+            
+  def initialize(name, length)
+    @name = name
+
+    
+  end
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  it "exists" do
+    cell = cell.new("B4")
+    expect(cell.new).to be_a(cell)
+  end
+
+  it "has a coordinate" do
+    cell = Cell.new("B4")
+  end
+
+  it "can contain a ship"do
+    cell = Cell.new("B4")
+    expect(cell.ship).to eq(nil)
+  end
+
+  it "can be empty" do
+    cell = Cell.new("B4")
+    expect(cell.empty?)to eq(true)
+  end
+
+  it "can place_ship" do
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.ship
+    expect(cell.empty?).to eq(false)
+  end
