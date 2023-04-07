d17531e831ade92618b333dd48b05637aef961cf
Lauren Golden <llgolden@gmail.com>
2023-04-06 21:42:59 -0600

    Add cell class methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,23 @@
 class Cell
   attr_reader :coordinate 
-              # :
             
   def initialize(coordinate)
     @coordinate = coordinate
+  end
 
-    
+  def ship
+    @nil
+  end
+
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(ship_name)
+    @ship = ship_name
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,22 +8,25 @@ RSpec.describe Cell do
     expect(cell).to be_a(Cell)
   end
 
-  xit "has a coordinate" do
+  it "has a coordinate" do
     cell = Cell.new("B4")
+    expect(cell.coordinate).to eq("B4")
   end
 
-  xit "can contain a ship"do
+  it "can contain a ship"do
     cell = Cell.new("B4")
     expect(cell.ship).to eq(nil)
   end
 
-  xit "can be empty" do
+  it "can be empty" do
     cell = Cell.new("B4")
     expect(cell.empty?).to eq(true)
   end
 
-  xit "can place_ship" do
+  it "can place_ship" do
+    cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
+    expect(cell.empty?).to eq(true)
     cell.place_ship(cruiser)
     cell.ship
     expect(cell.empty?).to eq(false)
