4d153afb90299bbd35d830a64e44761197e3a513
Lauren Golden <llgolden@gmail.com>
2023-04-06 21:57:11 -0600

    Add the remaining cell tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -26,9 +26,50 @@ RSpec.describe Cell do
   it "can place_ship" do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
+
     expect(cell.empty?).to eq(true)
     cell.place_ship(cruiser)
     cell.ship
     expect(cell.empty?).to eq(false)
   end
+
+  xit "can be fired upon" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+    expect(cell.ship.health).to eq(3)
+    expect(cell.fired_upon?).to eq(false)
+    cell.fire_upon
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
+  end
+
+  xit "can print to the board" do
+    cell_1 = Cell.new("B4")
+
+    expect(cell_1.render).to eq(".")
+    cell_1.fire_upon
+    expect(cell_1.render).to eq("M")
+  end
+
+  xit "has an option to show ships" do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
+
+ # Indicate that we want to show a ship with the optional argument
+
+    expect(cell_2.render(true)).to eq("S")
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to eq(false)
+    cruiser.hit
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(true)
+    expect(cell_2.render).to eq("X")
+  end
+
 end
