8565d5aa8477658e1863f9e3faba48695e6e9b79
Lauren Golden <llgolden@gmail.com>
2023-04-08 13:55:51 -0600

    Add place ship test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -67,6 +67,22 @@ RSpec.describe Board do
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
 
+  xit "can place a ship" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+   
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]   
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]   
+ 
+ 
+    expect(cell_1.ship).to be_a(Ship)
+    expect(cell_2.ship).to be_a(Ship)
+    expect(cell_3.ship).to be_a(Ship)
+    expect(cell_3.ship == cell_2.ship).to eq(true)
+  end
+
 end
 
 
